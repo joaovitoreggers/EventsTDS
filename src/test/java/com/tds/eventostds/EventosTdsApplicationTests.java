c879a646c867a3c66e2a4abf8c97c9f52bd71d7a
joaovitoreggers <joaoguchert195@gmail.com>
2025-06-06 14:38:55 -0300

    feat: initial commit

---
+++ src/test/java/com/tds/eventostds/EventosTdsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tds.eventostds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventosTdsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
